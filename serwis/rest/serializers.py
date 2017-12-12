a6c0765b6801b8caee53e5efc11f0f1b468bf419
Bartosz Ciesielczyk <Bartosz.Ciesielczyk@m.mbnd.eu>
2017-12-12 15:38:29 +0100

    serializers

--- serwis/rest/serializers.py
+++ serwis/rest/serializers.py
@@ -2,10 +2,20 @@
 from rest_framework import serializers
 from .models import Post
 
+class PostSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    text = serializers.CharField(max_length=10000)
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('text')
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+        return instance
+# class PostSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Post
+#         fields = ('text')
 
 
